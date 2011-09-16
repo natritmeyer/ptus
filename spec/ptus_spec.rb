ba71d16464d40d2597acbdf53dbed46007dad7af
Nat Ritmeyer <nat@natontesting.com>
2011-09-16 21:07:46 +0100

    adding before and after methods that channel standard output so it can be checked in the tests

--- spec/ptus_spec.rb
+++ spec/ptus_spec.rb
@@ -1,7 +1,17 @@
+require 'stringio'
 require 'ptus'
 
 describe "ptus" do
+  before(:each) do
+    @captured_output = StringIO.new
+    $stdout = @captured_output
+  end
+  
+  after(:each) do
+    $stdout = STDOUT
+  end
+  
   it "should still allow puts to work"
-  it "should print the string that is sent to it"
-  it "should print a message saying where the ptus was found"
+  it "should print the string that is sent to it" do end
+  it "should print a message saying where the ptus was found" do end
 end
