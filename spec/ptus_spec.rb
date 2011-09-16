bd33c0422599774e40a53313a3b7ba963b816167
Nat Ritmeyer <nat@natontesting.com>
2011-09-16 20:48:07 +0100

    adding pending tests

--- spec/ptus_spec.rb
+++ spec/ptus_spec.rb
@@ -1,1 +1,7 @@
 require 'ptus'
+
+describe "ptus" do
+  it "should still allow puts to work"
+  it "should print the string that is sent to it"
+  it "should print a message saying where the ptus was found"
+end
