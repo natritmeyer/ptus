94f9ad810212d4cca157a5c6a0e93ee9c84435d2
Nat Ritmeyer <nat@natontesting.com>
2011-09-22 23:18:01 +0100

    error message now written to stderr instead of stdout

--- lib/ptus.rb
+++ lib/ptus.rb
@@ -1,6 +1,6 @@
 module Kernel
   def ptus(input)
-    puts "Change 'ptus' to 'puts' here: #{caller.first}" 
+    warn "Change 'ptus' to 'puts' here: #{caller.first}" 
     puts input
   end
 end
--- spec/ptus_spec.rb
+++ spec/ptus_spec.rb
@@ -5,17 +5,28 @@ describe "ptus" do
   before(:each) do
     @test_string = "Hello"
     @captured_output = StringIO.new
+    @captured_error = StringIO.new
     $stdout = @captured_output
+    $stderr = @captured_error
   end
   
   after(:each) do
     $stdout = STDOUT
+    $stderr = STDERR
+  end
+
+  def output
+    close_and_read(@captured_output)
+  end
+
+  def error
+    close_and_read(@captured_error)
   end
   
-  def output
-    @captured_output.close_write
-    @captured_output.rewind
-    @captured_output.read
+  def close_and_read(stream)
+    stream.close_write
+    stream.rewind
+    stream.read
   end
   
   it "should still allow puts to work"do
@@ -30,6 +41,6 @@ describe "ptus" do
   
   it "should print a message saying where the ptus was found" do
     ptus @test_string
-    output.should match /Change 'ptus' to 'puts' here: .*spec\/ptus_spec.rb:\d+/
+    error.should match /Change 'ptus' to 'puts' here: .*spec\/ptus_spec.rb:\d+/
   end
 end
