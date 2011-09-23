0bb975994e0c0e64e2db5ed5c4796f12fd2c4ead
Nat Ritmeyer <nat@natontesting.com>
2011-09-23 11:43:02 +0100

    fixing whitespace

--- spec/ptus_spec.rb
+++ spec/ptus_spec.rb
@@ -9,7 +9,7 @@ describe "ptus" do
     $stdout = @captured_output
     $stderr = @captured_error
   end
-  
+
   after(:each) do
     $stdout = STDOUT
     $stderr = STDERR
@@ -22,23 +22,23 @@ describe "ptus" do
   def error
     close_and_read(@captured_error)
   end
-  
+
   def close_and_read(stream)
     stream.close_write
     stream.rewind
     stream.read
   end
-  
+
   it "should still allow puts to work"do
-     puts @test_string
-     output.should include @test_string
-   end
-   
+    puts @test_string
+    output.should include @test_string
+  end
+
   it "should print the string that is sent to it" do
     ptus @test_string
     output.should include @test_string
   end
-  
+
   it "should print a message saying where the ptus was found" do
     ptus @test_string
     error.should match /Change 'ptus' to 'puts' here: .*spec\/ptus_spec.rb:\d+/
